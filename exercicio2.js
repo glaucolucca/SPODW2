2abe065dff90236a71f439521cfebc1bdc239a6f
Glauco <glauco.lucca@toolsengenharia.com.br>
2025-03-17 08:18:06 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -3,7 +3,7 @@ function xadrez(num){
     for(let i = 0; i < num; i++){ 
         output += "# "; 
     } 
-    for (let i = 0; i < num; i++){ 
+    for(let i = 0; i < num; i++){ 
         if (i % 2 == 1) 
             console.log(output); 
         else 
@@ -12,3 +12,20 @@ function xadrez(num){
 }
 
 xadrez(8);
+
+#resolução
+function buildChessBoard (charToPrint, size) {
+    let result = "";
+
+    for (let row = 0; row < size; row++) {
+        for(let column = 0; colmun <= size; column++) {
+            if((row + columns) % 2 == 0){
+                result += " ";
+            } else {
+                result += charToPrint
+            }
+        }
+        result +="/n";
+    }
+    return result;
+}
