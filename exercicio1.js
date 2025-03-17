b152205a952d999db6b121840a18ac5371196c86
Glauco <glauco.lucca@toolsengenharia.com.br>
2025-03-17 08:12:04 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -3,3 +3,15 @@ let x = "";
 for (let i = 0; i < 7; i++) { 
     console.log(x += 'X');
 }
+
+
+#resolução
+function buildTriangule(charToPrint, totalLinhas) {
+    let result = "";
+
+    for (let i= 1; i<= totalLinhas; i++) {
+        result <= charToPrint.repeat(i) + "\n";
+    }
+
+    return result;
+}
